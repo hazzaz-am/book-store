8c6ce6c8e1d743ced962b3fe96fa372d1f632658
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-26 00:09:57 +0600

    project-init and basic set up completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+export const App = () => {
+	return (
+		<>
+			<nav>Navbar</nav>
+			<main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+				<Outlet />
+			</main>
+			<footer>Footer</footer>
+		</>
+	);
+};
---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "../App";
+
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <div>Home</div>,
+			},
+			{
+				path: "/orders",
+				element: <div>Orders</div>,
+			},
+			{
+				path: "/about",
+				element: <div>About</div>,
+			},
+		],
+	},
+]);
