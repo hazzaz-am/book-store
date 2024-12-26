5aaf2f37d5f94e5fb592fbe22fb15229a5236c68
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-26 09:46:08 +0600

    complete home page banner section

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import { Banner } from "./Banner";
+
+export const Home = () => {
+	return (
+		<>
+			<Banner />
+		</>
+	);
+};
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "../App";
+import { Orders } from "../pages/orders/Orders";
+import { Cart } from "../pages/cart/Cart";
+import { Checkout } from "../pages/checkout/Checkout";
+import { Home } from "../pages/home/Home";
 
 export const router = createBrowserRouter([
 	{
@@ -8,15 +12,23 @@ export const router = createBrowserRouter([
 		children: [
 			{
 				path: "/",
-				element: <div>Home</div>,
+				element: <Home />,
+			},
+			{
+				path: "/dashboard",
+				element: <Orders />,
 			},
 			{
 				path: "/orders",
-				element: <div>Orders</div>,
+				element: <Orders />,
 			},
 			{
-				path: "/about",
-				element: <div>About</div>,
+				path: "/cart",
+				element: <Cart />,
+			},
+			{
+				path: "/checkout",
+				element: <Checkout />,
 			},
 		],
 	},
