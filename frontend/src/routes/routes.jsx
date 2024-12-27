687c9e9f208afa488aab06d462f603a4a54fe49f
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-27 09:39:33 +0600

    add checkout page and form validation

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "../App";
 import { Orders } from "../pages/orders/Orders";
-import { Cart } from "../pages/cart/Cart";
-import { Checkout } from "../pages/checkout/Checkout";
 import { Home } from "../pages/home/Home";
 import { Login } from "../components/Login";
 import { Register } from "../components/Register";
+import { Cart } from "../pages/books/Cart";
+import { Checkout } from "../pages/books/Checkout";
 
 export const router = createBrowserRouter([
 	{
